781f0732c68007397eb5f1b423ffb047a3e46775
xuehao zhou <robertzhouxh@gmail.com>
2016-04-12 18:43:46 +0800

    add cask for emacs

--- emacs.sh
+++ emacs.sh
@@ -40,6 +40,10 @@ echo -e "\033[40;32m It will take a long time, just be patient! ... \033[0m"
 echo -e "\033[40;32m cd $HOME/.emacs.d \033[0m"
 cd $HOME/.emacs.d
 
+echo -e "\033[40;32m install the cask for emacs \033[0m"
+curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+
+echo -e "\033[40;32m install the plugins, this will take a long time ... \033[0m"
 cask install
 
 cd $CURRENT_DIR
